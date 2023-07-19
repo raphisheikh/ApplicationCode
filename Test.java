243028d464682e6632fa88fec1d5db2298262b59
raphisheikh <49804687+raphisheikh@users.noreply.github.com>
2023-07-19 17:59:19 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -3,7 +3,7 @@ public class Test {
 	    
 	    int first = 10;
 	    int second = 20;
-
+int second12 = 20;
 	    // add two numbers
 	    int sum = first + second;
 	    System.out.println(first + " + " + second + " = "  + sum);
